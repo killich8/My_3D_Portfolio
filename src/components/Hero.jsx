5a3e1ae2616925981e8d4093cef25aa8b8a48a03
Youness Killich <youness.killich8@gmail.com>
2024-11-07 22:29:50 +0100

    Update the repo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Youness </span>
+            Hi, I'm <span className='text-[#915EFF]'>Youness Killich </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           I'm a Data Scientist, <br className='sm:block hidden' />
